e4a47a8884022e461a9555f40f67892e99e7f560
Felipe De Andrade <felipe.deam98@gmail.com>
2025-12-26 22:54:08 -0300

    chore(icons): integrate build context and add debug flag to optimize task

--- gulp/tasks/icons/icons-optimize.js
+++ gulp/tasks/icons/icons-optimize.js
@@ -1,39 +1,36 @@
-// ← task
+// ← task to optimize SVGs.
 
 const gulp = require("gulp");
 const svgo = require("gulp-svgo");
 
-const paths = require("../../paths");
-const config = require("../../config");
-
 const { log } = require("../../utils/log");
 const { startTimer } = require("../../utils/timer");
+
+const { getBuildContext } = require("../utils/context");
+const ctx = getBuildContext();
+
+const outputDir = ctx.isDebug ? ctx.paths.icons.dist : ctx.paths.icons.temp;
 
 let timer;
 
 function logStart(cb) {
   timer = startTimer();
-  log.info(`Generating webfont: ${config.fontName}`);
-  log.verbose(`→ Source: ${paths.icons.src}`);
-  log.verbose(`→ Target: ${targetPath}`);
-  log.verbose(`→ Output directory: ${paths.icons.dist}`);
+  log.info(`Start optimizing SVGs!`);
+  log.verbose(`→ Source: ${ctx.paths.icons.src}`);
+  log.verbose(`→ Output directory: ${outputDir}`);
   cb();
 }
 logStart.displayName = "icons:start";
 
 function logEnd(cb) {
-  log.success(
-    `Finished generating webfont: ${config.fontName} (${timer.end()}) → ${paths.icons.dist}`,
-  );
-  log.info(`→ Total icons generate: ${glyphCount}`);
-  log.info(`→ Preview generate: ${previewPath}`);
+  log.success(`Finished optimizing SVGs! ${timer.end()} → ${outputDir}`);
   cb();
 }
 logEnd.displayName = "icons:end";
 
 function iconsOptimizeTask() {
   return gulp
-    .src(paths.icons.src)
+    .src(ctx.paths.icons.src, { allowEmpty: true })
     .pipe(
       svgo({
         plugins: [
@@ -42,17 +39,20 @@ function iconsOptimizeTask() {
         ],
       }),
     )
-    .pipe(gulp.dest(paths.icons.temp));
+    .pipe(gulp.dest(outputDir));
 }
-iconsOptimizeTask.displayName = "icons-optimize:run";
+iconsOptimizeTask.displayName = "icons:optimize:run";
 
 const iconsOptimize = gulp.series(logStart, iconsOptimizeTask, logEnd);
 
-iconsOptimize.displayName = "icons:font";
-iconsOptimize.description = "Generate icon font file.";
+iconsOptimize.displayName = "icons:optimize";
+iconsOptimize.description = "Optimize SVG icons before iconfont generation.";
 iconsOptimize.flags = {
-  "--silent": "Hides informational logs, showing only warnings and errors.",
+  "--debug": "Write outputs to the temp directory instead of dist.",
+  "--silence": "Hides informational logs, showing only warnings and errors.",
   "--verbose": "Shows detailed logs for debugging purposes.",
 };
+
+gulp.task(iconsOptimize.displayName, iconsOptimize);
 
 module.exports = { iconsOptimize };
