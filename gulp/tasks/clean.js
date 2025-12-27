71cba24cc0657d470c200cbbcb0b322a2d08120d
Felipe De Andrade <felipe.deam98@gmail.com>
2025-12-26 21:36:36 -0300

    chore(clean): migrate task to build context and standardize logs

--- gulp/tasks/clean.js
+++ gulp/tasks/clean.js
@@ -1,37 +1,36 @@
-// ← task to clear dist directory
+// ← task to clear dist and temp directory.
 
 const gulp = require("gulp");
 const clean = require("gulp-clean");
 
-const paths = require("../paths");
-
 const { log } = require("../utils/log");
 const { startTimer } = require("../utils/timer");
+
+const { getBuildContext } = require("../utils/context");
+const ctx = getBuildContext();
 
 let timer;
 
 function logStart(cb) {
   timer = startTimer();
-  log.info(`Start cleaning dist directory`);
-  log.verbose(`→ Cleaning: ${paths.dist}`);
+  log.info("Start cleaning build directories...");
+  log.verbose(`→ Cleaning: ${ctx.paths.dist}, ${ctx.paths.temp}`);
   cb();
 }
-logStart.displayName = "clean:start";
+logStart.displayName = "clean:log:start";
 
 function logEnd(cb) {
-  log.success(
-    `Finished cleaning dist directory! ${timer.end()} → ${paths.dist}`,
-  );
+  log.success(`Finished cleaning build directories! ${timer.end()}`);
   cb();
 }
-logEnd.displayName = "clean:end";
+logEnd.displayName = "clean:log:end";
 
 function cleanTask() {
   return gulp
-    .src([paths.dist], { read: false, allowEmpty: true })
+    .src([ctx.paths.dist, ctx.paths.temp], { read: false, allowEmpty: true })
     .pipe(clean())
     .on("error", (err) => {
-      log.error(`Cleaning dist directory failed: ${err.message}`);
+      log.error(`Cleaning build directories failed: ${err.message}`);
       throw err;
     });
 }
@@ -40,9 +39,9 @@ cleanTask.displayName = "clean:run";
 const cleanDefault = gulp.series(logStart, cleanTask, logEnd);
 
 cleanDefault.displayName = "clean";
-cleanDefault.description = "Clean (delete) the dist directory.";
+cleanDefault.description = "Clean (delete) build directories (dist and temp).";
 cleanDefault.flags = {
-  "--silent": "Hides informational logs, showing only warnings and errors.",
+  "--silence": "Hides informational logs, showing only warnings and errors.",
   "--verbose": "Shows detailed logs for debugging purposes.",
 };
 
