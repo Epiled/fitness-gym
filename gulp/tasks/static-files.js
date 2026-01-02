e17d365d5c92d075467b007525246404df5e5527
Felipe De Andrade <felipe.deam98@gmail.com>
2026-01-02 02:00:31 -0300

    chore(static): standardize variables and use build context

--- gulp/tasks/static-files.js
+++ gulp/tasks/static-files.js
@@ -5,38 +5,51 @@ const gulp = require("gulp");
 const { log } = require("../utils/log");
 const { startTimer } = require("../utils/timer");
 
+const { getBuildContext } = require("../utils/context");
+const ctx = getBuildContext();
+
+const srcDir = ctx.paths.src;
+
+const outputDir = ctx.paths.dist;
+
+let timer;
+
 function logStart(cb) {
   timer = startTimer();
   log.info("Start copy static files...");
-  log.verbose(`→ Source: ${ctx.paths.src}`);
-  log.verbose(`→ Output directory: ${ctx.paths.dist}`);
+  log.verbose(`→ Source: ${srcDir}`);
+  log.verbose(`→ Output directory: ${outputDir}`);
   cb();
 }
 logStart.displayName = "static:files:log:start";
 
 function logEnd(cb) {
-  log.success(
-    `Finished copying static files! ${timer.end()} → ${ctx.paths.dist}`,
-  );
+  log.success(`Finished copying static files! ${timer.end()} → ${outputDir}`);
   cb();
 }
 logEnd.displayName = "static:files:log:end";
 
 function staticFilesTask() {
   return gulp
-    .src([
-      `${ctx.paths.src}/**/*`,
+    .src(
+      [
+        `${srcDir}/**/*`,
 
-      // handled by html tasks
-      `!${ctx.paths.src}/html/**/*`,
+        // handled by html tasks
+        `!${srcDir}/html/**/*`,
 
-      // handled by css tasks
-      `!${ctx.paths.src}/css/**/*`,
+        // handled by css tasks
+        `!${srcDir}/css/**/*`,
 
-      // handled by icons pipeline (source SVGs)
-      `!${ctx.paths.src}/assets/svg/icons-ui/**/*`,
-    ])
-    .pipe(gulp.dest(ctx.paths.dist));
+        // handled by icons pipeline (source SVGs)
+        `!${srcDir}/assets/svg/icons-ui/**`,
+
+        // handled by images pipeline
+        `!${srcDir}/assets/img/**`,
+      ],
+      { base: srcDir, allowEmpty: true },
+    )
+    .pipe(gulp.dest(outputDir));
 }
 
 const staticFiles = gulp.series(logStart, staticFilesTask, logEnd);
