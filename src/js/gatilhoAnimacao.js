5162528dfa246a2683dca444d18e08af56cbd873
Felipe De Andrade <felipe.deam98@gmail.com>
2026-01-08 07:17:13 -0300

    perf(web): optimize font loading and JS execution

--- src/js/gatilhoAnimacao.js
+++
@@ -1,7 +0,0 @@
-import { contadorMarcados } from "./contador.js";
-import "./scroll-auto-flip.js";
-import "./scroll-reveal.js";
-
-const alturaDeAtivacao = window.innerHeight * 0.8;
-
-contadorMarcados.preparaContadores(alturaDeAtivacao);
---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+// import { contadorMarcados } from "./contador.js";
+
+window.addEventListener("load", () => {
+  import("./card-flip.js");
+  import("./tabs-indicator.js");
+  import("./scroll-reveal.js");
+  import("./scroll-to-top.js");
+  import("./scroll-auto-flip.js");
+});
+
+// const alturaDeAtivacao = window.innerHeight * 0.8;
+
+// contadorMarcados.preparaContadores(alturaDeAtivacao);
