f5bbd50e2e57cf3f976ccfd3bef2c070002db758
Felipe De Andrade <felipe.deam98@gmail.com>
2026-02-13 04:44:36 -0300

    refactor(js): split main bundle into main.js and inline.entry.js

---
+++ src/js/inline.entry.js
@@ -0,0 +1,3 @@
+import "./menu-toggle.js";
+import "./scroll-reveal.js";
+import "./scroll-counter.js";
--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,4 @@
-import("./menu-toggle.js");
-import("./scroll-reveal.js");
-
-window.addEventListener("load", () => {
-  import("./card-flip.js");
-  import("./tabs-indicator.js");
-  import("./scroll-to-top.js");
-  import("./scroll-auto-flip.js");
-  import("./scroll-counter.js");
-});
+import "./card-flip.js";
+import "./tabs-indicator.js";
+import "./scroll-auto-flip.js";
+import "./scroll-to-top.js";
