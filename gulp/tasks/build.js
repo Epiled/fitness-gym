9179b3576dd1f1432be34065cb05bbd207e990c9
Felipe De Andrade <felipe.deam98@gmail.com>
2026-02-18 20:15:41 -0300

    feat(build): integrate manifestCSSTAsk, jsCriticalInline and jsBuild into build flow

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -4,14 +4,20 @@ const gulp = require("gulp");
 
 const { cleanDefault } = require("./clean/clean");
 const { staticFiles } = require("./static-files");
+const { manifestCSSTask } = require("./manifest/manifest-css");
 const { cssBuild } = require("./css/css-build");
 const { htmlBuild } = require("./html/html-build");
+const { jsBuild } = require("./js/js-main");
 const { resizeBuild } = require("./resize/resize-build");
+const { jsCriticalInline } = require("./js/js-critical-inline");
 
 const build = gulp.series(
   cleanDefault,
   staticFiles,
+  manifestCSSTask,
   cssBuild,
+  jsCriticalInline,
+  jsBuild,
   htmlBuild,
   resizeBuild,
 );
