7252260a2b8ed525d24f6c83da1227f61b727078
Felipe De Andrade <felipe.deam98@gmail.com>
2026-02-23 12:58:27 -0300

    feat(build): add finalize stage to consolidate build artifacts

---
+++ gulp/tasks/finalize/finalize-html.js
@@ -0,0 +1,47 @@
+// ← task to finalize HTML output.
+
+const gulp = require("gulp");
+
+const { log } = require("../../utils/log");
+const { startTimer } = require("../../utils/timer");
+
+const { getBuildContext } = require("../../utils/context");
+const ctx = getBuildContext();
+
+const srcGlob = ctx.paths.html.glob;
+const srcDir = ctx.paths.html.dir;
+
+const tempDir = ctx.paths.html.temp.artifacts.gen.dir;
+const tempGlob = ctx.paths.html.temp.artifacts.gen.glob;
+
+const baseDir = ctx.isDebug ? srcDir : tempDir;
+
+const outputDir = ctx.paths.dist;
+
+let timer;
+
+function logStart(cb) {
+  timer = startTimer();
+  log.info("Start HTML finalization...");
+  cb();
+}
+logStart.displayName = "finalize:html:log:start";
+
+function logEnd(cb) {
+  log.success(`Finished HTML finalization! ${timer.end()} → ${outputDir}`);
+  cb();
+}
+logEnd.displayName = "finalize:html:log:end";
+
+const finalizeHtmlTask = function () {
+  return gulp
+    .src(tempGlob, { allowEmpty: true, base: baseDir })
+    .pipe(gulp.dest(outputDir));
+};
+finalizeHtmlTask.displayName = "finalize:html:run";
+
+const finalizeHtml = gulp.series(logStart, finalizeHtmlTask, logEnd);
+
+finalizeHtml.displayName = "finalize:html";
+
+module.exports = finalizeHtml;
