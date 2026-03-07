8c37622778b2310b1926f2862cd6b6952f6a6943
Felipe De Andrade <felipe.deam98@gmail.com>
2026-03-07 15:50:33 -0300

    refactor(html-inject-critical-css): fix input path, add validation and flag description

--- gulp/tasks/html/html-inject-critical-css.js
+++ gulp/tasks/html/html-inject-critical-css.js
@@ -1,4 +1,4 @@
-// ← task to inject CSS critical inline
+// ← task to inject CSS critical inline.
 
 const gulp = require("gulp");
 const replace = require("gulp-replace");
@@ -6,12 +6,13 @@ const fs = require("fs");
 
 const { log } = require("../../utils/log");
 const { startTimer } = require("../../utils/timer");
+const { fileExists } = require("../../utils/fileExists");
 
 const { getBuildContext } = require("../../utils/context");
 const ctx = getBuildContext();
 
-const tempDir = ctx.paths.css.temp.artifacts.gen.dir;
-const tempGlob = ctx.paths.html.temp.artifacts.gen.glob;
+const genDir = ctx.paths.css.temp.artifacts.gen.dir;
+const genGlob = ctx.paths.html.temp.artifacts.gen.glob;
 
 const outputDir = ctx.isDebug
   ? ctx.paths.dist
@@ -22,6 +23,9 @@ let timer;
 function logStart(cb) {
   timer = startTimer();
   log.info("Start insert critical CSS inline from source files...");
+  log.verbose(`→ Source glob: ${genGlob}`);
+  log.verbose(`→ Source dir: ${genDir}`);
+  log.verbose(`→ Output directory: ${outputDir}`);
   cb();
 }
 logStart.displayName = "html:inject:critical:css:log:start";
@@ -35,12 +39,17 @@ function logEnd(cb) {
 logEnd.displayName = "html:inject:critical:css:log:end";
 
 function htmlInjectCriticalCssTask() {
-  const criticalFile = fs
-    .readFileSync(`${tempDir}/critical.css`, "utf8")
-    .trim();
+  if (!ctx.isDebug && !fileExists(genDir)) {
+    log.warn(
+      `Temporary directory "${genDir}" does not exist. Please run the "prepare:css", "manifest:css" and "css:concat" task first to generate the necessary files before transforming images.`,
+    );
+    return Promise.reject();
+  }
+
+  const criticalFile = fs.readFileSync(`${genDir}/critical.css`, "utf8").trim();
 
   return gulp
-    .src(tempGlob, { allowEmpty: true })
+    .src(genGlob, { allowEmpty: true })
     .pipe(
       replace(
         /<!-- build:css:critical -->([\s\S]*?)<!-- end:build -->/g,
@@ -60,6 +69,12 @@ const htmlInjectCriticalCss = gulp.series(
 );
 
 htmlInjectCriticalCss.displayName = "html:inject:critical:css";
+htmlInjectCriticalCss.description =
+  "Inject critical CSS inline into HTML files. It reads the generated critical CSS from the temporary directory and replaces the corresponding build block in the HTML with a <style> tag containing the critical CSS. This ensures that critical styles are loaded immediately for better performance.";
+htmlInjectCriticalCss.flags = {
+  "--silence": "Hides informational logs, showing only warnings and errors.",
+  "--verbose": "Shows detailed logs for debugging purposes.",
+};
 
 gulp.task(htmlInjectCriticalCss.displayName, htmlInjectCriticalCss);
 
